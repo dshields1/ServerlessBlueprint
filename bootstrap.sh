024c8d3bcd980c8b7f5aac2308e99f926c3b5a4a
dylashie <dylashie@amazon.com>
2018-03-14 21:44:41 -0400

    Add GitHub repo option

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,23 @@ lowerProjectName="$(echo $projectName | tr '[A-Z]' '[a-z]')"
 stackSuffix='-FoundationStack'
 bucketSuffix='-deployment-bucket'
 
+read -p 'To use Github, enter `yes`: ' useGithub
+if [ "$useGithub" == "yes" ]; then
+    echo "Using Github!"
+else
+    echo "Using CodeCommit!"
+fi
+
 echo Creating $projectName$stackSuffix
-aws cloudformation --region us-east-1 create-stack --stack-name $projectName$stackSuffix --template-body file:///$PWD/aws-scaffold.yml --parameters ParameterKey=ProjectName,ParameterValue=$lowerProjectName --capabilities CAPABILITY_IAM
+if [ "$useGithub" == "yes" ]; then
+    read -p 'Github repo name: ' githubRepoName
+    read -p 'Github username: ' githubUsername
+    read -p 'Github token: ' githubToken
+    githubRepoUrl="https://github.com/$githubRepoName/$githubRepoName.git"
+    aws cloudformation --region us-east-1 create-stack --stack-name $projectName$stackSuffix --template-body file:///$PWD/aws-scaffold.yml --parameters ParameterKey=ProjectName,ParameterValue=$lowerProjectName ParameterKey=RepositoryType,ParameterValue=Github ParameterKey=GithubRepo,ParameterValue=$githubRepoName ParameterKey=GithubOwner,ParameterValue=$githubUsername ParameterKey=GithubToken,ParameterValue=$githubToken --capabilities CAPABILITY_IAM
+else
+    aws cloudformation --region us-east-1 create-stack --stack-name $projectName$stackSuffix --template-body file:///$PWD/aws-scaffold.yml --parameters ParameterKey=ProjectName,ParameterValue=$lowerProjectName --capabilities CAPABILITY_IAM
+fi
 
 echo Waiting for stack to finish
 aws cloudformation --region us-east-1 wait stack-create-complete --stack-name $projectName$stackSuffix
@@ -16,13 +31,16 @@ echo Stack created successfully
 
 echo Checking out source repo
 
-git config --global credential.helper '!aws codecommit credential-helper $@' 
-git config --global credential.UseHttpPath true 
-
-git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/$lowerProjectName
-
-cd $lowerProjectName
-git checkout -b master
+if [ "$useGithub" == "yes" ]; then
+    git clone $githubRepoUrl
+    cd $githubRepoName
+else
+    git config --global credential.helper '!aws codecommit credential-helper $@'
+    git config --global credential.UseHttpPath true
+    git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/$lowerProjectName
+    cd $lowerProjectName
+    git checkout -b master
+fi
 
 cp -r ../blueprint/* .
 
@@ -40,4 +58,8 @@ git push
 
 cd ..
 
-rm -r $lowerProjectName
+if [ "$useGithub" == "yes" ]; then
+    rm -r $githubRepoName
+else
+    rm -r $lowerProjectName
+fi
